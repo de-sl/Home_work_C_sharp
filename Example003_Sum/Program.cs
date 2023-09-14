6afa2fe14665c918358e827c9336d547c4634f13
de-sl <en060988@gmail.com>
2023-09-14 14:49:53 +0300

    Example003_Sum random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,11 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+int num1 = new Random().Next(1, 10); // Случайное число от 1 до 9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int result_random = num1 + num2;
+Console.Write("Summ random num = ");
+Console.Write(result_random);
